892a6225dcf54d48045c9562509e5dd17ef1993d
onisim_albarov <onisim93@gmail.com>
2022-11-29 17:04:07 +0400

    complete module4-loops-taskFinal

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,17 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
-
+        for (int y=0; y<cathetusLength;y++) {
+            for (int x=0; x<cathetusLength; x++) {
+                if (x >= cathetusLength-1-y) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,19 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-
+        int length = cathetusLength * 2 - 1;
+        int half = length / 2;
+        for (int y = 0; y < cathetusLength; y++) {
+            for (int x = 0; x < length; x++) {
+                if (x >= half - y && x <= half + y) {
+                    System.out.print(x <= half ? half+1-x : x - half + 1);
+                }
+                else if (x < half + y) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
